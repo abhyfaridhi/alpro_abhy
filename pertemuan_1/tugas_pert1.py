7204fac3168c3e1f0f6b6fd9e17fa4a58a6e55dc
abhyfaridhi <abhy1503@gmail.com>
2026-03-04 20:40:34 +0700

    Add files via upload

---
+++ pertemuan_1/tugas_pert1.py
@@ -0,0 +1,10 @@
+# Program menghitung keliling persegi
+
+# Input sisi
+sisi = float(input("Masukkan panjang sisi persegi: "))
+
+# Proses menghitung keliling
+keliling = 4 * sisi
+
+# Output hasil
+print("Keliling persegi adalah:", keliling)
---
+++ pertemuan_2/coding_pert2.py
@@ -0,0 +1,6 @@
+# Program Menghitung Luas Persegi Panjang
+
+panjang = float(input("Masukkan panjang: "))
+lebar = float(input("Masukkan lebar: "))
+
+luas = panjang * lebar
